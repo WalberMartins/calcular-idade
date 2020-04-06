0eb046d9be856a30a19ae0a5648e7bed1b2ec3e0
Walber Martins <walber.silvama@gmail.com>
2020-04-06 20:09:51 -0300

    sobreescrevendo o método adequado

--- src/servlet/CalculaIdade.java
+++ src/servlet/CalculaIdade.java
@@ -18,7 +18,7 @@ public class CalculaIdade extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		Calendar dataAtual = Calendar.getInstance();
 		Calendar dataNascimento = Calendar.getInstance();
